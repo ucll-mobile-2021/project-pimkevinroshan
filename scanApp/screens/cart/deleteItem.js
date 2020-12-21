0b8099c4465b7396824868fbb326bc1a07988682
Kevin Coorens <kevin.coorens@student.ucll.be>
2020-12-21 22:22:15 +0100

    delete button works

--- scanApp/screens/cart/deleteItem.js
+++ scanApp/screens/cart/deleteItem.js
@@ -3,7 +3,7 @@ const URI = 'http://mobiele.kc-productions.org/deleteItemFromCart.php?itemID=';
 export default {
     async deleteItem(barcode) {
         try {
-            let response = await fetch(URI + barcode, {method: "DELETE"});
+            let response = await fetch(URI + barcode);
             let responseJsonData = await response.json();
             return responseJsonData;
         }
